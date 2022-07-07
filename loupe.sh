443d1db40c2a7d0f53bd1d5e7accec25af1caea0
Mathias Couriol <macourio@z4r6p3.42lyon.fr>
2022-07-07 19:08:44 +0200

    C00 C01

---
+++ loupe.sh
@@ -0,0 +1,71 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    loupe.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: macourio <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/07/07 14:21:41 by macourio          #+#    #+#              #
+#    Updated: 2022/07/07 17:59:00 by macourio         ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/sh
+valid=1
+daycode=$1
+shift
+if [ -z $daycode ]
+then
+	echo "Erreur aucun code specifie. pour C00 le code est C00"
+	exit
+fi
+
+ext=""
+for i in $@
+do
+	ext=`echo "$i"|sed "s/.*\././"`
+	if [ ! $ext == '.c' ] 
+	then
+		valid=0
+	fi
+done
+
+if [ $valid ==  1 ]
+then
+	norm=`norminette -R CheckForbiddenSourceHeader $@|sed '/OK!$/d'|wc -l|tr -d ' '`
+	if [ $norm == '0' ]
+	then
+		echo "✅ Norminette passed !"
+	else
+		echo "⛔️ Norminette error !"
+		norminette -R CheckForbiddenSourceHeader $@
+		exit
+	fi
+	mkdir loupeEval
+	cd loupeEval
+	cp -r ~/loupe/loupe_$daycode .
+	count=0
+	for i in $@
+	do
+		gcc -Wall -Wextra -Werror ../$i loupe_$daycode/main$count.c
+		if [[ ! -f "a.out" ]]
+		then
+			echo "⛔️ Does not compile !"
+			exit
+		fi
+		./a.out > res
+		check=`diff res loupe_$daycode/check$count`
+		if [ -z $check ]
+		then
+			echo "✅ $i passed !"
+		else
+			echo "⛔️ $i failed !"
+			echo "$check"
+		fi
+		count=`expr $count + 1`
+	done
+	cd ..
+	rm -r loupeEval
+else
+	echo "⛔️ Presence de fichier non .c"
+fi
---
+++ loupe_C00/main0.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macourio <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 16:07:51 by macourio          #+#    #+#             */
+/*   Updated: 2022/07/07 16:32:52 by macourio         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int	main(void)
+{
+	ft_putchar('m');
+	ft_putchar('a');
+	ft_putchar('c');
+	ft_putchar('o');
+	ft_putchar('u');
+	ft_putchar('r');
+	ft_putchar('i');
+	ft_putchar('o');
+	ft_putchar(' ');
+	ft_putchar('!');
+}
